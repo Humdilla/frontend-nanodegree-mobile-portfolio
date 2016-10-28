dd2e6820bf8da352a044d567641f5d5198d65f18
Humdilla <humdilla94@yahoo.com>
2016-10-28 08:43:09 -0700

    Add image optimization to build script

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+@echo off
 :: Minify CSS
 del css\*.min.css
 for %%f in (css\*) do (
@@ -5,7 +6,7 @@ for %%f in (css\*) do (
 )
 del views\css\*.min.css
 for %%f in (views\css\*) do (
-  curl -X POST -s --data-urlencode input@%%f -o css\%%~nf.min.css https://cssminifier.com/raw
+  curl -X POST -s --data-urlencode input@%%f -o views\css\%%~nf.min.css https://cssminifier.com/raw
 )
 :: Minify JS
 del js\*.min.js
@@ -14,6 +15,10 @@ for %%f in (js\*) do (
 )
 del views\js\*.min.js
 for %%f in (views\js\*) do (
-  curl -X POST -s --data-urlencode input@%%f -o js\%%~nf.min.js https://javascript-minifier.com/raw
+  curl -X POST -s --data-urlencode input@%%f -o views\js\%%~nf.min.js https://javascript-minifier.com/raw
 )
 :: Optimize images
+del /q views\optimized_images\*
+magick convert -resize 720x540 -quality 33 views\images\pizzeria.jpg views\optimized_images\pizzeria.jpg
+magick convert views\images\pizza.png PNG8:views\optimized_images\pizza.png
+@echo on
